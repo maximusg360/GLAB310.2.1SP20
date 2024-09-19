086d5c433c3d1dd43be2949f1c8757ada4811437
Guzman <francisco.guzman@accenture.com>
2024-09-18 21:58:17 -0400

    Eureka

---
+++ SpringSoapdemo/src/main/java/com/example/SpringSoapdemo/SpringSoapdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSoapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapdemoApplication.class, args);
+	}
+
+}
---
+++ SpringSoapdemo/src/test/java/com/example/SpringSoapdemo/SpringSoapdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSoapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
